c38e1d2f472e46f230d3288ff5617aed02f2a4b1
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-05-24 18:59:15 -0300

    função getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  let status;
+  let colaboradores = data.employees.filter(elem => {
+   return elem.managers.find(elem => elem === id)
+  })
+  return status = colaboradores.length > 0
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  let pessoasLideradas = [];
+  if (isManager(managerId) === true) {
+    let colaboradores = data.employees.filter(elem => {
+      return elem.managers.find(elem => elem === managerId);
+      })
+      colaboradores.forEach(elem => {pessoasLideradas.push(`${elem.firstName} ${elem.lastName}`)});
+      return pessoasLideradas;
+  } else {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
