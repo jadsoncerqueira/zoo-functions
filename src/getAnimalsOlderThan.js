c2ff05c1e47d4ed01c0d3b8b706e055cd1a86bf9
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-05-24 16:10:29 -0300

    função getEmployeeByName

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,6 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // return data.species.every(elem => elem.name === animal  && elem.residents.age >= age)
   let classe = data.species.find(elem => elem.name === animal);
   return classe.residents.every(elem => elem.age >= age)
 }
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName) {
+    return data.employees.find(elem => elem.firstName === employeeName || elem.lastName === employeeName);
+  } else {
+    return {}
+  }
+  
 }
 
 module.exports = getEmployeeByName;
+
