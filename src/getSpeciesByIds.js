c4dbdb53c922c77e5f721af2ce668203162dae39
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-05-24 15:16:14 -0300

    função getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(ids) {
-  // seu código aqui
+  let animais = []
+  if (ids) {
+    let arg = Object.values(arguments);
+    arg.forEach(element1 => {
+      let aux = data.species.find(element2 => element2.id === element1 )
+      animais.push(aux)
+    });
+  }
+  return animais
 }
 
 module.exports = getSpeciesByIds;
