8579a6244bdb36d921a5e102f827367a71c573d6
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-05-24 20:33:06 -0300

    função countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  let relatorio = {}
+  if (animal) {
+    let animalRetornado = data.species.find(element => element.name === animal.specie)
+    if (animal.sex !== undefined){
+      animalRetornado = animalRetornado.residents.filter(elem => elem.sex === animal.sex)
+      return animalRetornado.length
+    } else {
+      return animalRetornado.residents.length
+    }
+  } else {
+    data.species.forEach(element => {
+      let nome = element.name
+      let totalSpecies = element.residents
+      relatorio[nome] = totalSpecies.length
+    })
+    return relatorio
+  }
 }
 
 module.exports = countAnimals;
