421d0b493f6e8db54f5f3f2dfd9fbe3d729aeb28
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-05-24 15:59:03 -0300

    função getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  // return data.species.every(elem => elem.name === animal  && elem.residents.age >= age)
+  let classe = data.species.find(elem => elem.name === animal);
+  return classe.residents.every(elem => elem.age >= age)
 }
 
 module.exports = getAnimalsOlderThan;
