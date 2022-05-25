0b44e0902a1390ff220eaeee3c1771980e270f95
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-05-24 21:23:10 -0300

    reorganizando codigo

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,16 +1,16 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  const colaboradores = data.employees.filter((elem) => 
-  elem.managers.find((elem2) => elem2 === id));
+  const colaboradores = data.employees.filter((elem) =>
+    elem.managers.find((elem2) => elem2 === id));
   return colaboradores.length > 0;
 }
 
 function getRelatedEmployees(managerId) {
   const pessoasLideradas = [];
   if (isManager(managerId) === true) {
-    const colaboradores = data.employees.filter((elem1) => 
-    elem1.managers.find((elem2) => elem2 === managerId));
+    const colaboradores = data.employees.filter((elem1) =>
+      elem1.managers.find((elem2) => elem2 === managerId));
     colaboradores.forEach((elem) => {
       pessoasLideradas.push(`${elem.firstName} ${elem.lastName}`);
     });
