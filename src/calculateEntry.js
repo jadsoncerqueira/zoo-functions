576f319808029f8b634b9284df592e854625c2d9
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-05-25 14:25:23 -0300

    organizando codigo

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,31 +1,31 @@
 const data = require('../data/zoo_data');
 
 const entrants = [
-	{ name:  'Lara Carvalho', age:  5 },
-	{ name:  'Frederico Moreira', age:  5 },
-	{ name:  'Pedro Henrique Carvalho', age:  5 },
-	{ name:  'Maria Costa', age:  18 },
-	{ name:  'Núbia Souza', age:  18 },
-	{ name:  'Carlos Nogueira', age:  50 },
+	{ name: 'Lara Carvalho', age: 5 },
+	{ name: 'Frederico Moreira', age: 5 },
+	{ name: 'Pedro Henrique Carvalho', age: 5 },
+	{ name: 'Maria Costa', age: 18 },
+	{ name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
 ];
 
 function countEntrants(entrants) {
-  let resultado = {
+  const resultado = {
     child: entrants.filter((elem) => elem.age < 18).length,
-    adult: entrants.filter((elem) => elem.age >= 18 && elem.age < 50 ).length,
-    senior: entrants.filter((elem) => elem.age >= 50).length
+    adult: entrants.filter((elem) => elem.age >= 18 && elem.age < 50).length,
+    senior: entrants.filter((elem) => elem.age >= 50).length,
   };
-  return resultado
+  return resultado;
 }
 
-function calculateEntry(entrants) {
-  if (entrants && Object.keys(entrants).length > 0) {
-    let entradas = countEntrants(entrants);
-    let valores = [ data.prices.child * entradas.child, data.prices.adult * entradas.adult,
-      data.prices.senior * entradas.senior] 
-    return valores.reduce((valorA, ValorB) => valorA + ValorB)
+function calculateEntry(en) {
+  if (en && Object.keys(en).length > 0) {
+    const entradas = countEntrants(en);
+    const valores = [data.prices.child * entradas.child, data.prices.adult * entradas.adult,
+      data.prices.senior * entradas.senior];
+    return valores.reduce((valorA, ValorB) => valorA + ValorB);
   }
-  return 0
+  return 0;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,29 +1,40 @@
 const data = require('../data/zoo_data');
 
+// função verifica animais do dia passado como parametro
+
+const verificaAnimais = (dia) => {
+  const animaisList = [];
+  const animais = data.species.filter((elem) => elem.availability.some((elem1) => elem1 === dia));
+  animais.forEach((elem) => animaisList.push(elem.name));
+  return animaisList;
+};
+
+// constante que guarda objeto com as chaves dos dias semana com seus respectivos horarios e animais em exibição
+
+const todosAnimais = {
+  Tuesday: { officeHour: `Open from ${data.hours.Tuesday.open}am until ${data.hours.Tuesday.close}pm`,
+  exhibition: verificaAnimais('Tuesday') },
+
+  Wednesday: { officeHour: `Open from ${data.hours.Wednesday.open}am until ${data.hours.Wednesday.close}pm`,
+  exhibition: verificaAnimais('Wednesday') },
+
+  Thursday: { officeHour: `Open from ${data.hours.Thursday.open}am until ${data.hours.Thursday.close}pm`,
+  exhibition: verificaAnimais('Thursday') },
+
+  Friday: { officeHour: `Open from ${data.hours.Friday.open}am until ${data.hours.Friday.close}pm`,
+  exhibition: verificaAnimais('Friday') },
+
+  Saturday: { officeHour: `Open from ${data.hours.Saturday.open}am until ${data.hours.Saturday.close}pm`,
+  exhibition: verificaAnimais('Saturday') },
+
+  Sunday: { officeHour: `Open from ${data.hours.Sunday.open}am until ${data.hours.Sunday.close}pm`,
+  exhibition: verificaAnimais('Sunday') },
+
+  Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
+};
+
 function getSchedule(scheduleTarget) {
-  
-  let diasSemana = Object.keys(data.hours);
-
-  // função verifica animais do dia passado como parametro
-
-  let verificaAnimais = (dia) => {
-    let animaisList = [];
-    let animais = data.species.filter((elem) => elem.availability.some((elem1) => elem1 === dia));
-    animais.forEach((elem) => animaisList.push(elem.name));
-    return animaisList;
-  };
-
-  // constante que guarda objeto com as chaves dos dias semana com seus respectivos horarios e animais em exibição
-
-  const todosAnimais = {
-    Tuesday: { officeHour: `Open from ${data.hours.Tuesday.open}am until ${data.hours.Tuesday.close}pm`, exhibition: verificaAnimais('Tuesday') },
-    Wednesday: { officeHour: `Open from ${data.hours.Wednesday.open}am until ${data.hours.Wednesday.close}pm`, exhibition: verificaAnimais('Wednesday') },
-    Thursday: { officeHour: `Open from ${data.hours.Thursday.open}am until ${data.hours.Thursday.close}pm`, exhibition: verificaAnimais('Thursday') },
-    Friday: { officeHour: `Open from ${data.hours.Friday.open}am until ${data.hours.Friday.close}pm`, exhibition: verificaAnimais('Friday') },
-    Saturday: { officeHour: `Open from ${data.hours.Saturday.open}am until ${data.hours.Saturday.close}pm`, exhibition: verificaAnimais('Saturday') },
-    Sunday: { officeHour: `Open from ${data.hours.Sunday.open}am until ${data.hours.Sunday.close}pm`, exhibition: verificaAnimais('Sunday') },
-    Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
-  };
+  const diasSemana = Object.keys(data.hours);
 
   // verifica se foi passado algum parametro caso contrario retorna a constante 'todosAnimais'
 
@@ -38,7 +49,7 @@ function getSchedule(scheduleTarget) {
       const dia = {};
       dia[scheduleTarget] = todosAnimais[scheduleTarget];
       return dia;
-    } 
+    }
     data.species.forEach((elem) => {
       if (elem.name === scheduleTarget) {
         diasSelecionado = elem.availability;
