f237c222702e7ba755e7e097e9c17695fc0d04c6
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-05-25 17:05:20 -0300

    função getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
+const localizaColaborador = (idColaborador) =>
+  data.employees.find((elem) => elem.id === idColaborador
+);
+
+const primeiroAnimalColaborador = (idPrimeiro) => localizaColaborador(idPrimeiro).responsibleFor[0];
+
+const idadeMaior = (idMaior) => {
+  let aux = 0;
+  let specieSelecionada = data.species.find((elem) => elem.id === idMaior);
+  specieSelecionada = Object.values(specieSelecionada.residents);
+  specieSelecionada.forEach((elem) => {
+    if (elem.age > aux) {
+      aux = elem.age;
+    }
+  })
+  return specieSelecionada.find((elem) => elem.age === aux)
+}
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const objRetornado = idadeMaior(primeiroAnimalColaborador(id))
+  return [objRetornado.name, objRetornado.sex, objRetornado.age]
 }
 
 module.exports = getOldestFromFirstSpecies;
